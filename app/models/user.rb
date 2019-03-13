7a7158768f70b4be4f499142c9aeaf0b2cb714da
Tosh Nandhan <snandhan@chicagobooth.edu>
2019-03-13 01:28:19 +0000

    Completed msm auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  validates :first_name, :presence => true
+  # validates :first_name, :presence => true
   
   # has_many :bookmarks, :class_name => "Bookmark", :foreign_key => "user_id"
   # Long form, achieves same as below
